24008e7c8761f8353575342a6f85b048a3bbbac8
Omaury Marmolejo <omaury@tango.io>
2022-12-29 16:00:29 -0600

    Adding filter for unfamiliar skill

--- beautiful-soup-scraping-practice-3/main.py
+++ beautiful-soup-scraping-practice-3/main.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+print('What skills do you want to learn?')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}...')
+
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
@@ -10,10 +14,11 @@ for job in jobs:
   if 'few' in published_date:
     company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
     skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+    more_info = job.header.h2.a['href']
 
-    print(f'''
-    Company Name: {company_name}
-    Required Skills: {skills}
-    ''')
+    if unfamiliar_skill not in skills:
+      print(f"Company Name: {company_name.strip()}")
+      print(f"Required Skills: {skills.strip()}")
+      print(f"More Info: {more_info}")
 
-    print('')
+      print('')
